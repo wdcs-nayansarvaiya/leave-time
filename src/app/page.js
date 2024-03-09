76af6405bb7a17174bb125775ac7433b8ecc02ac
wdcs-nayansarvaiya <nayan@webcluesinfotech.com>
2024-03-09 15:15:40 +0530

    add copyright in dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,10 @@ const HomePage = () => {
     <div>
       {/* <h1>Time Input Form</h1> */}
       <TimeInputForm />
+      <p style={{
+        textAlign: "center",
+        position: "relative",
+      }}>©2024 Nayan-Sarvaiya. All Rights Reserved</p>
     </div>
   );
 };
