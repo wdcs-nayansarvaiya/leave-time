13659ba1e3f1ba21f9bd2beb0cffc756666a0d40
wdcs-nayansarvaiya <nayan@webcluesinfotech.com>
2024-08-08 17:20:40 +0530

    Add analytics script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,28 @@
-import { Inter } from "next/font/google";
-import './globals.css'
-// import "../app/style.css";
-// import "../app/heckerForm.css"
+// import { Inter } from "next/font/google";
+// import './globals.css'
+// // import "../app/style.css";
+// // import "../app/heckerForm.css"
 
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Leave now",
+//   description: "Created by Nayan Sarvaiya",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
+
+import { Inter } from "next/font/google";
+import Head from "next/head"; // Import the Head component
+import "./globals.css";
+// import "../app/style.css";
+// import "../app/heckerForm.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +34,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+
+        {/* Google Analytics Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-dfgdfgdfg"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-sfdsdfsdfsf');
+          `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
